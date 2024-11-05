07bdc403ddeedb059739624b4f5f3f6d80a02ba3
Joao Neves <joaovictorneves28@hotmail.com.br>
2024-11-05 15:00:11 -0300

    inicializando projeto

---
+++ src/main/java/com/MV/DesafioSulWorkTech/DesafioSulWorkTechApplication.java
@@ -0,0 +1,13 @@
+package com.MV.DesafioSulWorkTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSulWorkTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSulWorkTechApplication.class, args);
+	}
+
+}
